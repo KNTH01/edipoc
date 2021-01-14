58632a875a49213dbedbb79f5413aa588846a67f
Kevin Nguyen <kevin.tuanhuy.nguyen@gmail.com>
2021-01-14 22:30:22 +0100

    Add editor.js

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
-    <QuillEditor  />
+    <QuillEditor class="py-12" />
+    <EditorJS class="py-12" />
   </div>
 </template>
 
 <script setup>
 import QuillEditor from "./components/editors/Quill.vue";
+import EditorJS from "./components/editors/EditorJS.vue";
 </script>
 
 <style></style>
---
+++ src/components/editors/EditorJS.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <h1 class="my-8 text-4xl font-semibold">Editor.js</h1>
+    <!-- Create the editor container -->
+    <div class="flex justify-between space-x-8">
+      <div class="w-1/2">
+        <div id="editorjs"></div>
+
+        <button class="my-6 button" @click="getContent">Get Content</button>
+      </div>
+
+      <pre class="w-1/2 overflow-auto pre-content">
+        {{ content }}
+      </pre>
+    </div>
+  </div>
+</template>
+
+<script>
+import EditorJS from "@editorjs/editorjs";
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+export default {
+  setup() {
+    const editor = ref(null);
+    const content = ref(null);
+
+    onMounted(() => {
+      editor.value = new EditorJS({ holder: "editorjs" });
+    });
+
+    onBeforeUnmount(() => {
+      if (editor.value) {
+        // editor.value.destroy();
+      }
+    });
+
+    return {
+      getContent() {
+        if (editor.value) {
+          editor.value
+            .save()
+            .then((outputData) => {
+              content.value = JSON.stringify(outputData);
+            })
+            .catch((error) => {
+              console.log("Saving failed: ", error);
+            });
+        }
+      },
+      content,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
